dd1e8b793d16b24dc373ff25915a6109755c7973
Alexandre <als.stumpf@gmail.com>
2025-03-30 20:53:44 -0300

    resolvendo bug das informações do usuário e introdusindo imagem de usuário

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,29 +1,34 @@
 function updateProfileInfo(profileData) {
-    const photo = document.getElementById('profile.photo');
-    photo.src = profileData.photo;
-    photo.alt = profileData.name;
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
 
-    const name = document.getElementById('profile.name');
-    name.innerText = profileData.name;
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
 
-    const job = document.getElementById('profile.job');
-    job.innerText = profileData.job;
-    job.href = profileData.linkedin;
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+    job.href = profileData.linkedin
+    job.target = '_blank'
 
-    const location = document.getElementById('profile.location');
-    location.innerText = profileData.location;
-    location.href = profileData.pin;
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+    location.href = profileData.pin
+    location.target = '_blank'
 
     const phone = document.getElementById('profile.phone');
     phone.innerText = profileData.phone;
-    phone.href = `tel:${profileData.phone}`;
+    phone.href = `tel:${profileData.phone}`
+    phone.target = '_blank'
 
-    const email = document.getElementById('profile.email');
-    email.innerText = profileData.email;
-    email.href = `mailto:${profileData.email}`;
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
+    email.target = '_blank'
 }
 
 (async () => {
-    const profileData = await fetchProfileData();
-    updateProfileInfo(profileData);
+    const profileData = await fetchProfileData()
+    updateProfileInfo(profileData)
+    console.log(profileData)
 })()
