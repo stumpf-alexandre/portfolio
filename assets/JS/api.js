d22e42649748199b790addeb11e9dfedc16f4a26
Alexandre Stumpf <als.stumpf@gmail.com>
2025-05-03 22:54:16 -0300

    Update api.js

--- assets/JS/api.js
+++ assets/JS/api.js
@@ -1,7 +1,7 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/stumpf-alexandre/portfolio/refs/heads/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/stumpf-alexandre/portfolio/refs/heads/main/data/profile.json'
 
-    const fetching = await fetch(url);
+    const fetching = await fetch(url)
 
     return fetching.json()
 }
